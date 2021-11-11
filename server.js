2b1a2a1d76b4feed29e82f358edcaf313290598b
Savion Elsey-Williams <s.e.williams2001@gmail.com>
2021-11-10 23:43:55 -0500

    deploying

--- server.js
+++ server.js
@@ -13,11 +13,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout', 
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // routes
 app.use(require('./routes/api.js'));
