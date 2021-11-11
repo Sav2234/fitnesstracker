ff19b7d2b0013a4bd31657e5597613749ca9c854
Savion Elsey-Williams <s.e.williams2001@gmail.com>
2021-11-10 23:53:09 -0500

    fixing html file working

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Workout = require('../models/workOut');
+const Workout = require('../models/workOut.js');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
