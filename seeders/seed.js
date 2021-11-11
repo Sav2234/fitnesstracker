24c9c7027991460ae0cfe48bd3bee2b6a777c5ce
Savion Elsey-Williams <s.e.williams2001@gmail.com>
2021-11-11 09:58:41 -0500

    fixed path to models

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Workout = require('../models/workOut');
+const Workout = require('../models/workout');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
