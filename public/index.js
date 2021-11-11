fc46fe0e5da303a59ce64f4c7a0920f448952f69
Savion Elsey-Williams <s.e.williams2001@gmail.com>
2021-11-11 10:20:02 -0500

    fixed path to models

--- public/index.js
+++ public/index.js
@@ -1,16 +1,13 @@
 init();
 
 async function init() {
-  let workout;
-
   if (location.search.split("=")[1] === undefined) {
-    workout = await API.getLastWorkout()
-    console.log(workout)
-  }
-  if (workout) {
-    location.search = "?id=" + workout._id;
-  } else {
-    document.querySelector("#continue-btn").classList.add("d-none")
+    const workout = await API.getLastWorkout();
+    if (workout) {
+      location.search = "?id=" + workout._id;
+    } else {
+      document.querySelector("#continue-btn").classList.add("d-none")
+    }
   }
 }
 
